c182bdcb66f814b6aa9ad3ddfdddd216eff57071
advert31337 <advert31337@yandex.ru>
2020-06-04 11:00:03 +0300

    Added base framework

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
-
+import {Excel} from '@/components/excel/Excel'
+import {Toolbar} from './components/toolbar/Toolbar'
+import {Header} from './components/header/Header'
+import {Formula} from './components/formula/Formula'
+import {Table} from './components/table/Table'
 import './scss/index.scss'
 
 
-console.log('excel in start')
+const excel = new Excel('#app', {
+  components: [Header, Toolbar, Formula, Table]
+})
+
+excel.render()
