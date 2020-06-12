9eb15238f1363e9bf045970060ad6e96a0c78ff1
advert31337 <advert31337@yandex.ru>
2020-06-13 01:30:14 +0300

    added redux and table logic

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
+import {storage, debounce} from '@core/utils'
+import {createStore} from '@core/createStore'
 import {Excel} from '@/components/excel/Excel'
 import {Toolbar} from './components/toolbar/Toolbar'
 import {Header} from './components/header/Header'
 import {Formula} from './components/formula/Formula'
 import {Table} from './components/table/Table'
 import './scss/index.scss'
+import {rootReducer} from './redux/rootReducer'
+import {initialState} from './redux/initialState'
 
+const store = createStore(rootReducer, initialState)
+
+const stateListener = debounce(state => {
+  console.log('App State: ', state)
+  storage('excel-state', state)
+}, 300)
+
+store.subscribe(stateListener)
 
 const excel = new Excel('#app', {
-  components: [Header, Toolbar, Formula, Table]
+  components: [Header, Toolbar, Formula, Table],
+  store
 })
 
 excel.render()
